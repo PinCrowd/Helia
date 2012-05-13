e6a55d8bcd4eb8ea16bbca823e54f8879be2410e
Robert Allen <zircote@gmail.com>
2012-05-13 14:05:11 -0500

    more API worx yo

--- application/modules/v1/controllers/Game/GamesIdController.php
+++ application/modules/v1/controllers/Game/GamesIdController.php
@@ -2,5 +2,41 @@
 
 class V1_Game_GamesIdController extends Pincrowd_Rest_AbstractController
 {
-
+    /**
+     *
+     * @var string
+     */
+    protected $_baseUri = '/v1';
+    /**
+     *
+     * @var string
+     */
+    protected $_resource = 'games';
+    /**
+     *
+     * @var V1_Service_Game_Games
+     */
+    protected $_service;
+    /**
+     * Default Allow
+     * @var array
+     */
+    protected $_allow = array('GET','POST', 'OPTIONS', 'HEAD');
+    /**
+     *
+     * @see Pincrowd_Rest_AbstractController::init()
+     */
+    public function init()
+    {
+        parent::init();
+        $this->setService(new V1_Service_Game_Game($this->_options, $this));
+        /* @var $bootstrap Pincrowd_Rest_Bootstrap_Bootstrap */
+        $bootstrap = Zend_Controller_Front::getInstance()->getParam('bootstrap');
+        if($bootstrap->hasResource('db')){
+            $this->_service->getMapper()->setDb($bootstrap->getResource('db'));
+        }
+        if($bootstrap->hasResource('log')){
+            $log = $bootstrap->getResource('log');
+        }
+    }
 }
